ed0c1d41ea96e77298599171132b5995ce99636f
sanjay krishna <sanjay krishna@LAPTOP-14QGKHI0>
2022-06-16 20:02:38 +0530

    Adding reverse string program at 16th date

---
+++ src/test/java/collectionFramework/StringReverse.java
@@ -0,0 +1,11 @@
+package collectionFramework;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		String s="sanjeeva";
+		for(int i=s.length()-1;i>=0;i--) {
+			System.out.print(s.charAt(i));
+		}
+	}
+}
